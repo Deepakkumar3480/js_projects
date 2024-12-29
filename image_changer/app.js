a9f7adcb80d06f752a4f503ab25b825177a8c59c
Deepak kumar <kumardeepak3480@gmail.com>
2024-12-29 23:02:12 +0530

    image changer js project

---
+++ image_changer/app.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll('.css input')
+console.log(inputs)
+inputs.forEach((input)=>{
+    input.addEventListener('change',update)
+})
+inputs.forEach((input)=>{
+    input.addEventListener('mousemove',update)
+})
+
+function update(){
+    const change = this.dataset.sizing;
+    document.documentElement.style.setProperty(
+        `--${this.name}`,this.value+change
+    )
+}
